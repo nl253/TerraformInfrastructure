c1e8585184a42e4e3116056552168486d5c347f6
Norbert Logiewa <norbertlogiewa96@gmail.com>
2020-05-12 19:53:20 +0100

    Add vars for cloudwatch alarm

---
+++ aws-cloudwatch-alarm/variables.tf
@@ -0,0 +1,39 @@
+variable "app_name" {
+  type    = string
+  default = "testapp123"
+}
+
+variable "threshold" {
+  type    = number
+  default = 10
+}
+
+variable "period" {
+  type    = number
+  default = 120
+}
+
+variable "unit" {
+  type    = string
+  default = null
+}
+
+variable "stat" {
+  type    = string
+  default = "Average"
+}
+
+variable "metric" {
+  default = "BucketSizeBytes"
+  type    = string
+}
+
+variable "service" {
+  default = "AWS/S3"
+  type    = string
+}
+
+variable "env" {
+  default = "dev"
+  type    = string
+}
