2a459aa14f430285b66eb158033cbe3d573cc659
Norbert Logiewa <norbertlogiewa96@gmail.com>
2020-05-25 14:19:54 +0100

    Remove defaults from gcp-service-account module

--- gcp-service-account/main.tf
+++ gcp-service-account/main.tf
@@ -2,38 +2,21 @@ provider "google-beta" {
   project = "test-project-277710"
 }
 
-terraform {
-  backend "gcs" {
-    bucket = "ci-nl"
-    prefix = "service-accounts/service-account"
-  }
-}
-
 resource "google_service_account" "service_account" {
-  account_id   =  replace(lower(var.name), " ", "-")
+  lifecycle {
+    prevent_destroy = true
+  }
+  account_id   = replace(lower(var.name), " ", "-")
   display_name = var.name
+  project      = var.project
 }
-
-//data "google_iam_policy" "policy" {
-//  audit_config {
-//    service = "allServices"
-//    audit_log_configs {
-//      log_type = "DATA_WRITE"
-//    }
-//    audit_log_configs {
-//      log_type = "ADMIN_READ"
-//    }
-//  }
-//  count = length(var.roles)
-//}
-
 
 resource "google_project_iam_binding" "binding" {
   members = [
     "serviceAccount:${google_service_account.service_account.email}"
   ]
-  role = var.roles[count.index]
-  count = length(var.roles)
+  role    = var.roles[count.index]
+  count   = length(var.roles)
   project = var.project
 }
 
--- gcp-service-account/variables.tf
+++ gcp-service-account/variables.tf
@@ -1,17 +1,11 @@
 variable "name" {
-  default = "terraform"
+  type = string
 }
 
 variable "roles" {
-  default = [
-    "roles/editor",
-    "roles/storage.admin",
-    "roles/iam.securityAdmin",
-    "roles/storage.objectAdmin",
-  ]
   type = list(string)
 }
 
 variable "project" {
-  default = "test-project-277710"
+  type = string
 }
