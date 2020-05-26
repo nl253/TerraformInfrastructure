b47caec98f8f73cc1f4e31a1fd811a6e95f13d27
Norbert Logiewa <norbertlogiewa96@gmail.com>
2020-05-26 19:44:57 +0100

    Add AWS mgmt trail

---
+++ aws-cloudtrail/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region  = "eu-west-2"
+  profile = "ma"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "codebuild-nl"
+    key    = "cloudtrail/mgmt/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+variable "region" {
+  default = "eu-west-2"
+  type    = string
+}
+
+resource "aws_cloudtrail" "trail" {
+  enable_log_file_validation    = true
+  enable_logging                = true
+  name                          = "management-trail"
+  include_global_service_events = true
+  is_multi_region_trail         = false
+  is_organization_trail         = false
+  s3_bucket_name                = "management-trail-nl"
+  tags                          = {}
+}
